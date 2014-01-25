8effd927836ef6c4b3a18f5fa67448040d4acca7
Benjamin Botwin <thecodethinker@gmail.com>
2014-01-24 23:18:29 -0500

    Changed main game class

---
+++ GGJ2014/src/com/game/jam/MyGame.java
@@ -0,0 +1,35 @@
+package com.game.jam;
+
+import com.badlogic.gdx.Game;
+
+
+public class MyGame extends Game {
+
+	
+	@Override
+	public void create() {	
+		
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+	@Override
+	public void render() {		
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
